5aefc1e9952398ebbb2be6d0ce4148bbf0ceb863
Kun Feng <fkengun@gmail.com>
2022-07-15 16:18:00 -0500

    Add logging functions as macros

---
+++ ChronoAPI/ChronoLog/include/log.h
@@ -0,0 +1,36 @@
+//
+// Created by kfeng on 4/5/22.
+//
+
+#ifndef CHRONOLOG_LOG_H
+#define CHRONOLOG_LOG_H
+
+#pragma once
+
+#ifdef __cplusplus
+#include <cstdio>
+#include <cstring>
+#else
+#include <stdio.h>
+#include <string.h>
+#endif
+
+// Truncates the full __FILE__ path, only displaying the basename
+#define __FILENAME__ \
+    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+
+#define LOG(fmt, ...)           do{ printf(fmt "\n", ##__VA_ARGS__); } while(0)
+#define LOGT(tag, fmt, ...)     do{ printf(tag ": " fmt "\n", ##__VA_ARGS__); } while(0)
+#define LOGI(fmt, ...)          do{ printf("%s: " fmt "\n", __FILENAME__, ##__VA_ARGS__); } while(0)
+#define LOGE(fmt, ...)          do{ printf("\033[31mERROR: %s: %s: %d: " fmt "\033[m\n", \
+                                    __FILENAME__, __func__, __LINE__, ##__VA_ARGS__); \
+                                } while(0)
+
+#define NDEBUG 1
+#ifdef NDEBUG
+#define LOGD(fmt, ...)          ((void)0)
+#else
+#define LOGD(fmt, ...)          do{ printf("\033[33m%s: " fmt "\033[m\n", __FILENAME__, ##__VA_ARGS__); } while(0)
+#endif
+
+#endif //CHRONOLOG_LOG_H

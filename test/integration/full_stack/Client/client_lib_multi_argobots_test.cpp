d476aae7b304f0ce0d03696d4b39d9c064d52baf
aparnasasidharan2017 <aparnasasidharan2017@gmail.com>
2023-01-16 09:00:06 -0600

    argobots_client

--- test/integration/full_stack/Client/client_lib_multi_argobots_test.cpp
+++ test/integration/full_stack/Client/client_lib_multi_argobots_test.cpp
@@ -6,6 +6,9 @@
 #include <thread>
 #include <abt.h>
 
+#define CHRONICLE_NAME_LEN 32
+#define STORY_NAME_LEN 32
+
 ChronoLogClient *client;
 
 struct thread_arg
@@ -14,8 +17,9 @@ struct thread_arg
 
 };
 
-void thread_function(void *t)
+void thread_function(void *tt)
 {
+	struct thread_arg *t = (struct thread_arg*)tt;
 
 	std::string server_ip = "127.0.0.1";
 	int base_port = 5555;
@@ -25,6 +29,28 @@ void thread_function(void *t)
         int flags = 0;
 	uint64_t offset;
 	int ret = client->Connect(server_uri,client_id,flags,offset);
+	std::string chronicle_name;
+        if(t->tid%2==0) chronicle_name = "gscs5er9TcdJ9mOgUDteDVBcI0oQjozK";
+	else chronicle_name = "6RPkwqX2IOpR41dVCqmWauX9RfXIuTAp";
+        std::unordered_map<std::string, std::string> chronicle_attrs;
+        chronicle_attrs.emplace("Priority", "High");
+        chronicle_attrs.emplace("IndexGranularity", "Millisecond");
+        chronicle_attrs.emplace("TieringPolicy", "Hot");
+        ret = client->CreateChronicle(chronicle_name, chronicle_attrs, flags);
+	flags = 1;
+	ret = client->AcquireChronicle(chronicle_name,flags);
+	ret = client->ReleaseChronicle(chronicle_name,flags);
+	std::string story_name = gen_random(STORY_NAME_LEN);
+	std::unordered_map<std::string, std::string> story_attrs;
+        story_attrs.emplace("Priority", "High");
+        story_attrs.emplace("IndexGranularity", "Millisecond");
+        story_attrs.emplace("TieringPolicy", "Hot");
+        flags = 2;
+        ret = client->CreateStory(chronicle_name, story_name, story_attrs, flags);
+	ret = client->AcquireStory(chronicle_name,story_name,flags);
+        ret = client->ReleaseStory(chronicle_name,story_name,flags);
+	ret = client->DestroyStory(chronicle_name,story_name,flags);
+	ret = client->DestroyChronicle(client_id,flags);
 	ret = client->Disconnect(client_id,flags);
 }
 

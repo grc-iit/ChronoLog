a878f398d3f2641a3ce890f91501d4c09a6da238
Kun Feng <kfeng1@hawk.iit.edu>
2023-08-09 22:36:22 -0500

    storyteller_test gets configuration from configuration file as visor and keeper executables

--- Client/storyteller_test/storyteller_test.cpp
+++ Client/storyteller_test/storyteller_test.cpp
@@ -19,19 +19,24 @@ uint64_t get_chrono_timestamp()
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <address> <provider_id>" << std::endl;
-        exit(0);
-    }
-    tl::engine myEngine("ofi+sockets", THALLIUM_CLIENT_MODE);
+//    if (argc != 3)
+//    {
+//        std::cerr << "Usage: " << argv[0] << " <address> <provider_id>" << std::endl;
+//        exit(0);
+//    }
+    ChronoLog::ConfigurationManager confManager("./default_conf.json");
+    std::string proto_conf = confManager.RPC_CONF.CLIENT_VISOR_CONF.PROTO_CONF.string();
+    tl::engine myEngine(proto_conf, THALLIUM_CLIENT_MODE);
     tl::remote_procedure record_event = myEngine.define("record_event");
-    tl::endpoint server = myEngine.lookup(argv[1]);
-    uint16_t provider_id = atoi(argv[2]);
+    std::string visor_uri = proto_conf + "://"
+                            + confManager.RPC_CONF.CLIENT_VISOR_CONF.VISOR_END_CONF.VISOR_IP.string() + ":"
+                            + std::to_string(confManager.RPC_CONF.CLIENT_VISOR_CONF.VISOR_END_CONF.VISOR_BASE_PORT);
+    tl::endpoint server = myEngine.lookup(visor_uri);
+    uint16_t provider_id = confManager.RPC_CONF.CLIENT_VISOR_CONF.VISOR_END_CONF.SERVICE_PROVIDER_ID;
 
 
 
-    //chronolog::StorytellerClient storytellerClient(myEngine, argv[1], provider_id);
+//    chronolog::StorytellerClient storytellerClient(myEngine, argv[1], provider_id);
 
 
     for (int i = 0; i < 10; ++i)
@@ -41,7 +46,7 @@ int main(int argc, char **argv)
         chl::LogEvent event(1, log_time, 7, i, "line_" + std::to_string(i));
         std::cout << "generated_event {" << event.storyId << ":" << event.time() << ":" << event.clientId << ":"
                   << event.index() << "}" << std::endl;
-//	recordingClient->send_event_msg(event);
+//        recordingClient->send_event_msg(event);
     }
 
 

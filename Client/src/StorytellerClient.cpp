d7bf1af64aeae9c6df49c6a237e3a9ad2c113e08
Kun Feng <kfeng1@hawk.iit.edu>
2023-10-05 17:45:35 -0500

    Enable direct run (non-interactive) mode w/ event recording
    Refactor the interactive mode code to be easier to understand

--- Client/src/StorytellerClient.cpp
+++ Client/src/StorytellerClient.cpp
@@ -73,6 +73,7 @@ int chronolog::StoryWritingHandle<KeeperChoicePolicy>::log_event(std::string con
     // INNA: make send event returm 0 in case of tl RPC failure ....
     keeperRecordingClient->send_event_msg(log_event);
 
+    // Kun: do we have any reason to have a different errno definition than what we already have in errcode.h?
     return 1;
 }
 /////////////////////

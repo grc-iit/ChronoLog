1e12bbcceb3ccedf151eefdd93005268db782577
Kun Feng <kfeng1@hawk.iit.edu>
2023-10-05 17:45:35 -0500

    Enable direct run (non-interactive) mode w/ event recording
    Refactor the interactive mode code to be easier to understand

--- Client/ChronoAdmin/client_admin.cpp
+++ Client/ChronoAdmin/client_admin.cpp
@@ -33,18 +33,209 @@ Created by Aparna on 01/12/2023
 #include <cassert>
 #include <unistd.h>
 #include <pwd.h>
+#include <getopt.h>
+#include <functional>
+
+typedef struct workload_conf_args_ {
+    uint64_t chronicle_count = 1;
+    uint64_t story_count = 1;
+    uint64_t min_event_size = 0;
+    uint64_t ave_event_size = 256;
+    uint64_t max_event_size = 512;
+    uint64_t event_count = 1;
+    std::string event_input_file;
+    bool interactive = false;
+} workload_conf_args;
+
+std::pair<std::string, workload_conf_args> cmd_arg_parse(int argc, char **argv)
+{
+    int opt;
+    char *config_file = nullptr;
+    workload_conf_args workload_args;
+
+    // Define the long options and their corresponding short options
+    struct option long_options[] = {
+            {"config", required_argument, nullptr, 'c'},
+            {"chronicle_count", required_argument, nullptr, 'h'},
+            {"story_count", required_argument, nullptr, 't'},
+            {"min_event_size", required_argument, nullptr, 'a'},
+            {"ave_event_size", required_argument, nullptr, 's'},
+            {"max_event_size", required_argument, nullptr, 'b'},
+            {"event_count", required_argument, nullptr, 'n'},
+            {"event_input_file", optional_argument, nullptr, 'f'},
+            {"interactive", optional_argument, nullptr, 'i'},
+            {nullptr, 0,                  nullptr, 0} // Terminate the options array
+    };
+
+    // Parse the command-line options
+    while ((opt = getopt_long(argc, argv, "h:t:c:a:s:b:n:f:i", long_options, nullptr)) != -1)
+    {
+        switch (opt)
+        {
+            case 'c':
+                config_file = optarg;
+                break;
+            case 'h':
+                workload_args.chronicle_count = strtoll(optarg, nullptr, 10);
+                break;
+            case 't':
+                workload_args.story_count = strtoll(optarg, nullptr, 10);
+                break;
+            case 'a':
+                workload_args.min_event_size = strtoll(optarg, nullptr, 10);
+                break;
+            case 's':
+                workload_args.ave_event_size = strtoll(optarg, nullptr, 10);
+                break;
+            case 'b':
+                workload_args.max_event_size = strtoll(optarg, nullptr, 10);
+                break;
+            case 'n':
+                workload_args.event_count = strtoll(optarg, nullptr, 10);
+                break;
+            case 'f':
+                workload_args.event_input_file = optarg;
+                break;
+            case 'i':
+                workload_args.interactive = true;
+                break;
+            case '?':
+                // Invalid option or missing argument
+                LOGE("Usage: %s -c|--config <config_file>\n"
+                     "-h|--chronicle_count <chronicle_count>\n"
+                     "-t|--story_count <story_count>\n"
+                     "-a|--min_event_size <min_event_size>\n"
+                     "-s|--ave_event_size <ave_event_size>\n"
+                     "-b|--max_event_size <max_event_size>\n"
+                     "-n|--event_count <event_count>\n"
+                     "-f|--input <event_input_file>\n"
+                     "-i|--interactive", argv[0]);
+                exit(EXIT_FAILURE);
+            default:
+                // Unknown option
+                LOGE("Unknown option: %c\n", opt);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    // Check if the config file option is provided
+    if (config_file)
+    {
+        LOGI("Config file specified: %s\n", config_file);
+        if (workload_args.interactive) {
+            LOGI("Interactive mode on");
+        }
+        else
+        {
+            LOGI("Interactive mode off");
+            LOGI("Chronicle count: %lu", workload_args.chronicle_count);
+            LOGI("Story count: %lu", workload_args.story_count);
+            if (!workload_args.event_input_file.empty())
+            {
+                LOGI("Event input file specified: %s", workload_args.event_input_file.c_str());
+            }
+            else
+            {
+                LOGI("No event input file specified, use default/specified separate conf args ...");
+                LOGI("Min event size: %lu", workload_args.min_event_size);
+                LOGI("Ave event size: %lu", workload_args.ave_event_size);
+                LOGI("Max event size: %lu", workload_args.max_event_size);
+                LOGI("Event count: %lu", workload_args.event_count);
+            }
+        }
+        return {std::pair<std::string, workload_conf_args>((config_file), workload_args)};
+    }
+    else
+    {
+        LOGI("No config file specified, using default instead\n");
+        return {};
+    }
+}
+
+void test_create_chronicle(chronolog::Client &client, const std::string &chronicle_name)
+{
+    int ret, flags = 0;
+    std::unordered_map<std::string, std::string> chronicle_attrs;
+    chronicle_attrs.emplace("Priority", "High");
+    chronicle_attrs.emplace("IndexGranularity", "Millisecond");
+    chronicle_attrs.emplace("TieringPolicy", "Hot");
+    ret = client.CreateChronicle(chronicle_name, chronicle_attrs, flags);
+    assert(ret == CL_SUCCESS || ret == CL_ERR_CHRONICLE_EXISTS);
+}
+
+chronolog::StoryHandle *test_acquire_story(chronolog::Client &client,
+                                           const std::string &chronicle_name,
+                                           const std::string &story_name)
+{
+    int flags = 0;
+    std::unordered_map<std::string, std::string> story_acquisition_attrs;
+    story_acquisition_attrs.emplace("Priority", "High");
+    story_acquisition_attrs.emplace("IndexGranularity", "Millisecond");
+    story_acquisition_attrs.emplace("TieringPolicy", "Hot");
+    std::pair<int, chronolog::StoryHandle *> acq_ret = client.AcquireStory(chronicle_name, story_name,
+                                                                           story_acquisition_attrs,
+                                                                           flags);
+    assert(acq_ret.first == CL_SUCCESS || acq_ret.first == CL_ERR_ACQUIRED);
+    return acq_ret.second;
+}
+
+void test_write_event(chronolog::StoryHandle *story_handle, const std::string &event_payload)
+{
+    int ret = story_handle->log_event(event_payload);
+    assert(ret == 1);
+}
+
+void test_release_story(chronolog::Client &client, const std::string &chronicle_name, const std::string &story_name)
+{
+    int ret = client.ReleaseStory(chronicle_name, story_name);
+    assert(ret == CL_SUCCESS || ret == CL_ERR_NOT_EXIST);
+}
+
+void test_destroy_chronicle(chronolog::Client &client, const std::string &chronicle_name)
+{
+    int ret = client.DestroyChronicle(chronicle_name);
+    assert(ret == CL_SUCCESS || ret == CL_ERR_NOT_EXIST || ret == CL_ERR_ACQUIRED);
+}
+
+void test_destroy_story(chronolog::Client &client, const std::string &chronicle_name, const std::string &story_name)
+{
+    int ret = client.DestroyStory(chronicle_name, story_name);
+    assert(ret == CL_SUCCESS || ret == CL_ERR_NOT_EXIST || ret == CL_ERR_ACQUIRED);
+}
+
+// function to extract and execute commands from command line input
+void command_dispatcher(const std::string& command_line,
+                        std::unordered_map<std::string, std::function<void(std::vector<std::string>&)>> command_map) {
+    const char *delim = " ";
+    std::vector<std::string> command_subs;
+    char *s = std::strtok((char *) command_line.c_str(), delim);
+    command_subs.emplace_back(s);
+    while (s != nullptr)
+    {
+        s = std::strtok(nullptr, delim);
+        if (s != nullptr)
+            command_subs.emplace_back(s);
+    }
+    if (command_map.find(command_subs[0]) != command_map.end()) {
+        command_map[command_subs[0]](command_subs);
+    } else {
+        std::cout << "Invalid command. Please try again." << std::endl;
+    }
+}
 
 int main(int argc, char **argv)
 {
     std::string default_conf_file_path = "./default_conf.json";
-    std::string conf_file_path;
-    conf_file_path = parse_conf_path_arg(argc, argv);
+    std::pair<std::string, workload_conf_args> cmd_args = cmd_arg_parse(argc, argv);
+    std::string conf_file_path = cmd_args.first;
+    workload_conf_args workload_args = cmd_args.second;
     if (conf_file_path.empty())
     {
         conf_file_path = default_conf_file_path;
     }
     ChronoLog::ConfigurationManager confManager(conf_file_path);
     chronolog::Client client(confManager);
+    chronolog::StoryHandle *story_handle;
 
     int flags = 0;
     int ret;
@@ -61,104 +252,120 @@ int main(int argc, char **argv)
 
     std::cout << " connected to server address : " << server_uri << std::endl;
 
-    std::cout << "Metadata operations: \n"
-              << "\t-c <chronicle_name> , create a Chronicle <chronicle_name> \n"
-              << "\t-a -s <chronicle_name> <story_name>, acquire Story <story_name> in Chronicle <chronicle_name> \n"
-              << "\t-r -s <chronicle_name> <story_name>, release Story <story_name> in Chronicle <chronicle_name> \n"
-              << "\t-d -s <chronicle_name> <story_name>, destroy Story <story_name> in Chronicle <chronicle_name> \n"
-              << "\t-d -c <chronicle_name>, destroy Chronicle <chronicle_name> \n"
-              << "\t-disconnect \n" << std::endl;
-
-
-    std::vector<std::string> commands;
-
-    std::string command_line;
-
-    const char *delim = " ";
-
-    std::string str;
-
-    flags = 0;
-
-    while (true)
+    if (workload_args.interactive)
     {
-        str.clear();
-        std::getline(std::cin, str);
-        if (str == "-disconnect") break;
+        std::cout << "Metadata operations: \n"
+                  << "\t-c <chronicle_name> , create a Chronicle <chronicle_name>\n"
+                  << "\t-a -s <chronicle_name> <story_name>, acquire Story <story_name> in Chronicle <chronicle_name>\n"
+                  << "\t-w <event_string>, write Event with <event_string> as payload\n"
+//                  << "\t-r <event_id>, read Event <event_id>"
+                  << "\t-q -s <chronicle_name> <story_name>, release Story <story_name> in Chronicle <chronicle_name>\n"
+                  << "\t-d -s <chronicle_name> <story_name>, destroy Story <story_name> in Chronicle <chronicle_name>\n"
+                  << "\t-d -c <chronicle_name>, destroy Chronicle <chronicle_name>\n"
+                  << "\t-disconnect\n" << std::endl;
 
-        std::vector<std::string> command_subs;
+        std::unordered_map<std::string, std::function<void(std::vector<std::string> &)>> command_map = {
+                {"-c", [&](std::vector<std::string> &command_subs)
+                       {
+                           assert(command_subs.size() == 2);
+                           std::string chronicle_name = command_subs[1];
+                           test_create_chronicle(client, chronicle_name);
+                       }},
+                {"-a", [&](std::vector<std::string> &command_subs)
+                       {
+                           if (command_subs[1] == "-s")
+                           {
+                               assert(command_subs.size() == 4);
+                               std::string chronicle_name = command_subs[2];
+                               std::string story_name = command_subs[3];
+                               story_handle = test_acquire_story(client, chronicle_name, story_name);
+                           }
+                       }},
+                {"-q", [&](std::vector<std::string> &command_subs)
+                       {
+                           if (command_subs[1] == "-s")
+                           {
+                               assert(command_subs.size() == 4);
+                               std::string chronicle_name = command_subs[2];
+                               std::string story_name = command_subs[3];
+                               test_release_story(client, chronicle_name, story_name);
+                           }
+                       }},
+                {"-w", [&](std::vector<std::string> &command_subs)
+                       {
+                           assert(command_subs.size() == 2);
+                           std::string event_payload = command_subs[1];
+                           test_write_event(story_handle, event_payload);
+                       }},
+                {"-d", [&](std::vector<std::string> &command_subs)
+                       {
+                           if (command_subs[1] == "-c")
+                           {
+                               assert(command_subs.size() == 3);
+                               std::string chronicle_name = command_subs[2];
+                               test_destroy_chronicle(client, chronicle_name);
+                           }
+                           else if (command_subs[1] == "-s")
+                           {
+                               assert(command_subs.size() == 4);
+                               std::string chronicle_name = command_subs[2];
+                               std::string story_name = command_subs[3];
+                               test_destroy_story(client, chronicle_name, story_name);
+                           }
+                       }}
+        };
 
-        char *s = std::strtok((char *) str.c_str(), delim);
-        command_subs.emplace_back(s);
-
-        while (s != nullptr)
+        std::string command_line;
+        while (true)
         {
-            s = std::strtok(nullptr, delim);
-            if (s != nullptr)
-                command_subs.emplace_back(s);
+            command_line.clear();
+            std::getline(std::cin, command_line);
+            if (command_line == "-disconnect") break;
+            command_dispatcher(command_line, command_map);
         }
+    }
+    else
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution char_dist(0, 255);
+        double sigma = (workload_args.max_event_size - workload_args.min_event_size) / 6;
+        std::normal_distribution<double> size_dist(workload_args.ave_event_size, sigma);
+        for (uint64_t i = 0; i < workload_args.chronicle_count; i++)
+        {
+            // create chronicle test
+            std::string chronicle_name = "chronicle_" + std::to_string(i);
+            test_create_chronicle(client, chronicle_name);
 
-        if (command_subs[0] == "-c")
-        {
-            assert(command_subs.size() == 2);
-            std::string chronicle_name = command_subs[1];
-            std::unordered_map<std::string, std::string> chronicle_attrs;
-            chronicle_attrs.emplace("Priority", "High");
-            chronicle_attrs.emplace("IndexGranularity", "Millisecond");
-            chronicle_attrs.emplace("TieringPolicy", "Hot");
-            ret = client.CreateChronicle(chronicle_name, chronicle_attrs, flags);
-            assert(ret == CL_SUCCESS || ret == CL_ERR_CHRONICLE_EXISTS);
-        }
-        else if (command_subs[0] == "-a")
-        {
-            if (command_subs[1] == "-s")
+            uint64_t story_count_per_chronicle = workload_args.story_count / workload_args.chronicle_count;
+            for (uint64_t j = 0; j < story_count_per_chronicle; j++)
             {
-                assert(command_subs.size() == 4);
-                std::string chronicle_name = command_subs[2];
-                std::string story_name = command_subs[3];
-                std::unordered_map<std::string, std::string> story_acquisition_attrs;
-                story_acquisition_attrs.emplace("Priority", "High");
-                story_acquisition_attrs.emplace("IndexGranularity", "Millisecond");
-                story_acquisition_attrs.emplace("TieringPolicy", "Hot");
-                std::pair<int, chronolog::StoryHandle *> acq_ret = client.AcquireStory(chronicle_name, story_name,
-                                                                                       story_acquisition_attrs,
-                                                                                       flags);
-                assert(acq_ret.first == CL_SUCCESS || acq_ret.first == CL_ERR_ACQUIRED);
+                // acquire story test
+                std::string story_name = "story_" + std::to_string(j);
+                story_handle = test_acquire_story(client, chronicle_name, story_name);
+
+                // write event test
+                uint64_t event_count_per_story = workload_args.event_count / workload_args.story_count;
+                for (uint64_t k = 0; k < event_count_per_story; k++)
+                {
+                    uint64_t event_size = std::min(std::max(size_dist(gen), workload_args.min_event_size * 1.0),
+                                                   workload_args.max_event_size * 1.0);
+                    std::string event_payload;
+                    for (uint64_t l = 0; l < event_size; l++)
+                        event_payload += std::to_string('a' + std::abs(char_dist(gen)) + 1);
+                    test_write_event(story_handle, event_payload);
+                }
+
+                // release story test
+                test_release_story(client, chronicle_name, story_name);
+
+                // destroy story test
+                test_destroy_story(client, chronicle_name, story_name);
             }
+
+            // destroy chronicle test
+            test_destroy_chronicle(client, chronicle_name);
         }
-        else if (command_subs[0] == "-r")
-        {
-            if (command_subs[1] == "-s")
-            {
-                assert(command_subs.size() == 4);
-                std::string chronicle_name = command_subs[2];
-                std::string story_name = command_subs[3];
-                ret = client.ReleaseStory(chronicle_name, story_name);
-                assert(ret == CL_SUCCESS || ret == CL_ERR_NOT_EXIST);
-            }
-
-        }
-        else if (command_subs[0] == "-d")
-        {
-            if (command_subs[1] == "-c")
-            {
-                assert(command_subs.size() == 3);
-                std::string chronicle_name = command_subs[2];
-                ret = client.DestroyChronicle(chronicle_name);
-                assert(ret == CL_SUCCESS || ret == CL_ERR_NOT_EXIST || ret == CL_ERR_ACQUIRED);
-            }
-            else if (command_subs[1] == "-s")
-            {
-                assert(command_subs.size() == 4);
-                std::string chronicle_name = command_subs[2];
-                std::string story_name = command_subs[3];
-                ret = client.DestroyStory(chronicle_name, story_name);
-                assert(ret == CL_SUCCESS || ret == CL_ERR_NOT_EXIST || ret == CL_ERR_ACQUIRED);
-            }
-
-        }
-
-
     }
 
     ret = client.Disconnect();

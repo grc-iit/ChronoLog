7d86eeb3c6906f149fbc7c7109a4903b24bdc2ee
Eneko Gonzalez <egonzalez30@iit.edu>
2023-11-20 12:11:42 -0600

    STYLE: reformat code for /external_libs/test

--- external_libs/test/cereal/cereal_test.cpp
+++ external_libs/test/cereal/cereal_test.cpp
@@ -13,97 +13,111 @@
 #define NUM_CHRONOKEEPER 3
 #define SERDE_BUF_SIZE (1024 * 1024)
 
-struct ChronoKeeperInfo {
+struct ChronoKeeperInfo
+{
     std::string ip_;
     int port_;
 
     template <class Archive>
-    void serialize(Archive & ar) {
+    void serialize(Archive &ar)
+    {
         ar(ip_, port_);
     }
 
-    ChronoKeeperInfo() : ip_("0.0.0.0"), port_(0) {}
+    ChronoKeeperInfo(): ip_("0.0.0.0"), port_(0)
+    {}
 
-    ChronoKeeperInfo(std::string ip, int port) : ip_(std::move(ip)), port_(port) {}
+    ChronoKeeperInfo(std::string ip, int port): ip_(std::move(ip)), port_(port)
+    {}
 
-    std::string toString() const {
+    std::string toString() const
+    {
         return "(" + ip_ + ":" + std::to_string(port_) + ")";
     }
 };
 
-struct TimeInfo {
+struct TimeInfo
+{
     std::string timestamp_;
     double driftRate_;
 
     template <class Archive>
-    void serialize(Archive & ar) {
+    void serialize(Archive &ar)
+    {
         ar(timestamp_, driftRate_);
     }
 
-    TimeInfo() : driftRate_(0) {}
+    TimeInfo(): driftRate_(0)
+    {}
 
-    TimeInfo(std::string timestamp, double driftRate) : timestamp_(std::move(timestamp)), driftRate_(driftRate) {}
+    TimeInfo(std::string timestamp, double driftRate): timestamp_(std::move(timestamp)), driftRate_(driftRate)
+    {}
 
-    std::string toString() const {
+    std::string toString() const
+    {
         return "(" + timestamp_ + ", " + std::to_string(driftRate_) + ")";
     }
 };
 
-struct ClientMessage {
-    enum ClientMessageType {
-        UNKNOWN = -1,
-        CONNECTION = 0,
-        DISCONNECTION = 1,
-        GETTIMEINFO = 2
+struct ClientMessage
+{
+    enum ClientMessageType
+    {
+        UNKNOWN = -1, CONNECTION = 0, DISCONNECTION = 1, GETTIMEINFO = 2
     };
 
     ClientMessageType msgType_;
 
     template <class Archive>
-    void serialize(Archive & ar) {
+    void serialize(Archive &ar)
+    {
         ar(msgType_);
     }
 
-    ClientMessage() { msgType_ = ClientMessageType::UNKNOWN; }
+    ClientMessage()
+    { msgType_ = ClientMessageType::UNKNOWN; }
 
-    std::string toString() const {
+    std::string toString() const
+    {
         return "Type: " + std::to_string(msgType_);
     }
 };
 
-struct ServerMessage {
-    enum ServerMessageType {
-        UNKNOWN = -1,
-        CONNRESPONSE = 0,
-        DISCONNRESPONSE = 1,
-        GETTIMEINFORESPONSE = 2,
-        GETCHRONOKEEPERRESPONSE = 3
+struct ServerMessage
+{
+    enum ServerMessageType
+    {
+        UNKNOWN = -1, CONNRESPONSE = 0, DISCONNRESPONSE = 1, GETTIMEINFORESPONSE = 2, GETCHRONOKEEPERRESPONSE = 3
     };
 
     ServerMessageType msgType_;
     TimeInfo timeInfo_;
-    std::vector<ChronoKeeperInfo> chronoKeeperList_;
+    std::vector <ChronoKeeperInfo> chronoKeeperList_;
 
     template <class Archive>
-    void serialize(Archive & ar) {
+    void serialize(Archive &ar)
+    {
         ar(msgType_, timeInfo_, chronoKeeperList_);
     }
 
-    ServerMessage() : msgType_(ServerMessageType::UNKNOWN), timeInfo_(TimeInfo()), chronoKeeperList_() {}
+    ServerMessage(): msgType_(ServerMessageType::UNKNOWN), timeInfo_(TimeInfo()), chronoKeeperList_()
+    {}
 
-    std::string toString() {
+    std::string toString()
+    {
         std::string chronoKeeperListStr;
-        for (const ChronoKeeperInfo& info : chronoKeeperList_) {
-            if (!chronoKeeperListStr.empty()) chronoKeeperListStr += ", ";
+        for(const ChronoKeeperInfo &info: chronoKeeperList_)
+        {
+            if(!chronoKeeperListStr.empty()) chronoKeeperListStr += ", ";
             chronoKeeperListStr += info.toString();
         }
-        return "Type: " + std::to_string(msgType_)
-                + ", timeinfo: " + timeInfo_.toString() + ", ChronoKeeper list: " + chronoKeeperListStr;
+        return "Type: " + std::to_string(msgType_) + ", timeinfo: " + timeInfo_.toString() + ", ChronoKeeper list: " +
+               chronoKeeperListStr;
     }
 };
 
-unsigned char *shared_msg_buffer_unsigned = nullptr;
-char  *shared_msg_buffer_default = nullptr;
+unsigned char*shared_msg_buffer_unsigned = nullptr;
+char*shared_msg_buffer_default = nullptr;
 std::string shared_msg_string;
 
 int main()
@@ -111,7 +125,7 @@ int main()
     std::string clientMsgFile = "/tmp/client_msg";
     std::string serverMsgFile = "/tmp/server_msg";
     std::chrono::high_resolution_clock::time_point t1, t2;
-    std::chrono::duration<double> duration{};
+    std::chrono::duration <double> duration{};
 
     //
     // file-based serialization
@@ -120,7 +134,7 @@ int main()
     // client ====>>>>
     t1 = std::chrono::high_resolution_clock::now();
     {
-        std::ofstream os(clientMsgFile, std::ios::binary | std::ios::trunc);
+        std::ofstream os(clientMsgFile, std::ios::binary|std::ios::trunc);
         cereal::BinaryOutputArchive archive(os);
 
         ClientMessage clientMsg;
@@ -130,7 +144,7 @@ int main()
         std::cout << "Msg to server: " << clientMsg.toString() << std::endl;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to serialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     // server <<<<====
@@ -145,19 +159,20 @@ int main()
         std::cout << "Msg from client: " << clientMsg.toString() << std::endl;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to deserialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     // server ====>>>>
     t1 = std::chrono::high_resolution_clock::now();
     {
-        std::ofstream os(serverMsgFile, std::ios::binary | std::ios::trunc);
+        std::ofstream os(serverMsgFile, std::ios::binary|std::ios::trunc);
         cereal::BinaryOutputArchive archive(os);
 
         ServerMessage serverMsg;
         TimeInfo timeInfo(std::string("1234567890"), 0.00001111);
-        std::vector<ChronoKeeperInfo> chronoKeeperList;
-        for (int i = 0; i < NUM_CHRONOKEEPER; i++) {
+        std::vector <ChronoKeeperInfo> chronoKeeperList;
+        for(int i = 0; i < NUM_CHRONOKEEPER; i++)
+        {
             ChronoKeeperInfo chronoKeeperInfo(std::string("172.25.101.") + std::to_string(i + 1), 8000 + i);
             chronoKeeperList.push_back(chronoKeeperInfo);
         }
@@ -169,7 +184,7 @@ int main()
         std::cout << "Msg to client: " << serverMsg.toString() << std::endl;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to serialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     // client <<<<====
@@ -184,14 +199,14 @@ int main()
         std::cout << "Msg from server: " << serverMsg.toString() << std::endl;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to deserialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     //
     // memory-based serialization
     //
     std::cout << "*** Memory-based serialization ***" << std::endl;
-    shared_msg_buffer_unsigned = (unsigned char *) malloc(SERDE_BUF_SIZE);
+    shared_msg_buffer_unsigned = (unsigned char*)malloc(SERDE_BUF_SIZE);
     // client ====>>>>
     t1 = std::chrono::high_resolution_clock::now();
     {
@@ -207,14 +222,15 @@ int main()
         memcpy(shared_msg_buffer_unsigned, oss.str().c_str(), oss.str().length());
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to serialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     // server <<<<====
     t1 = std::chrono::high_resolution_clock::now();
     {
         std::istringstream iss;
-        iss.rdbuf()->pubsetbuf(reinterpret_cast<char*>(const_cast<unsigned char*>(shared_msg_buffer_unsigned)), SERDE_BUF_SIZE);
+        iss.rdbuf()->pubsetbuf(reinterpret_cast<char*>(const_cast<unsigned char*>(shared_msg_buffer_unsigned))
+                               , SERDE_BUF_SIZE);
         cereal::BinaryInputArchive archive(iss);
 
         ClientMessage clientMsg;
@@ -223,7 +239,7 @@ int main()
         std::cout << "Msg from client: " << clientMsg.toString() << std::endl;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to deserialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     // server ====>>>>
@@ -234,8 +250,9 @@ int main()
 
         ServerMessage serverMsg;
         TimeInfo timeInfo(std::string("1234567890"), 0.00001111);
-        std::vector<ChronoKeeperInfo> chronoKeeperList;
-        for (int i = 0; i < NUM_CHRONOKEEPER; i++) {
+        std::vector <ChronoKeeperInfo> chronoKeeperList;
+        for(int i = 0; i < NUM_CHRONOKEEPER; i++)
+        {
             ChronoKeeperInfo chronoKeeperInfo(std::string("172.25.101.") + std::to_string(i + 1), 8000 + i);
             chronoKeeperList.push_back(chronoKeeperInfo);
         }
@@ -249,15 +266,14 @@ int main()
         memcpy(shared_msg_buffer_default, oss.str().c_str(), oss.str().length());
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to serialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     // client <<<<====
     t1 = std::chrono::high_resolution_clock::now();
     {
         std::istringstream iss;
-        iss.rdbuf()->pubsetbuf(
-                reinterpret_cast<char*>(const_cast<char*>(shared_msg_buffer_default)), SERDE_BUF_SIZE);
+        iss.rdbuf()->pubsetbuf(reinterpret_cast<char*>(const_cast<char*>(shared_msg_buffer_default)), SERDE_BUF_SIZE);
         cereal::BinaryInputArchive archive(iss);
 
         ServerMessage serverMsg;
@@ -266,7 +282,7 @@ int main()
         std::cout << "Msg from server: " << serverMsg.toString() << std::endl;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    duration = std::chrono::duration_cast <std::chrono::duration <double>>(t2 - t1);
     std::cout << "Time used to deserialize: " << duration.count() * 1000 << " ms" << std::endl;
 
     free(shared_msg_buffer_unsigned);
--- external_libs/test/rocksdb/rocksDB_test_mt.cpp
+++ external_libs/test/rocksdb/rocksDB_test_mt.cpp
@@ -15,63 +15,65 @@
 
 using namespace std;
 
-void writer_thread_func() {
-    rocksdb::DB* db;
+void writer_thread_func()
+{
+    rocksdb::DB*db;
     rocksdb::Options options;
     options.create_if_missing = true;
 
-    rocksdb::Status status =
-            rocksdb::DB::Open(options, "/tmp/testdb", &db);
+    rocksdb::Status status = rocksdb::DB::Open(options, "/tmp/testdb", &db);
     assert(status.ok());
 
     // Insert values
-    for (int i = 0; i < NUM_KEYS; i++) {
-        status = db->Put(rocksdb::WriteOptions(),
-                         "Test key" + to_string(i),
-                         "Test value" + to_string(i + 1));
+    for(int i = 0; i < NUM_KEYS; i++)
+    {
+        status = db->Put(rocksdb::WriteOptions(), "Test key" + to_string(i), "Test value" + to_string(i + 1));
         assert(status.ok());
     }
 }
 
-void reader_thread_func() {
-    std::this_thread::sleep_for (std::chrono::seconds(5));
+void reader_thread_func()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 
-    rocksdb::DB* db;
+    rocksdb::DB*db;
     rocksdb::Options options;
     options.create_if_missing = true;
 
-    rocksdb::Status status =
-            rocksdb::DB::OpenForReadOnly(options, "/tmp/testdb", &db);
+    rocksdb::Status status = rocksdb::DB::OpenForReadOnly(options, "/tmp/testdb", &db);
     assert(status.ok());
 
     auto start = chrono::steady_clock::now();
 
     // Read back value
     std::string value;
-    for (int i = 0; i < NUM_KEYS; i++) {
+    for(int i = 0; i < NUM_KEYS; i++)
+    {
         status = db->Get(rocksdb::ReadOptions(), "Test key" + to_string(i), &value);
         assert(status.ok());
         assert(!status.IsNotFound());
     }
 
     auto end = chrono::steady_clock::now();
-    cout << "Elapsed time in milliseconds: "
-         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+    cout << "Elapsed time in milliseconds: " << chrono::duration_cast <chrono::milliseconds>(end - start).count()
          << " ms" << endl;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char**argv)
+{
     thread writer_thr, reader_thr[NUM_READER_THRDS];
 
     writer_thr = thread(writer_thread_func);
 
-    for (int i = 0; i < NUM_READER_THRDS; i++) {
+    for(int i = 0; i < NUM_READER_THRDS; i++)
+    {
         reader_thr[i] = thread(reader_thread_func);
     }
 
     writer_thr.join();
 
-    for (int i = 0; i < NUM_READER_THRDS; i++) {
+    for(int i = 0; i < NUM_READER_THRDS; i++)
+    {
         reader_thr[i].join();
     }
 }

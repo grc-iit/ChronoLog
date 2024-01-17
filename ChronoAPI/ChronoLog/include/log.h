f5d5861e292c5a2ed5503f0ae3ba82def7ff1f3d
Eneko Gonzalez <egonzalez30@iit.edu>
2024-01-17 22:34:02 +0100

    FIX: Destructor of the logger class is public now.

--- ChronoAPI/ChronoLog/include/log.h
+++ ChronoAPI/ChronoLog/include/log.h
@@ -65,9 +65,9 @@ public:
      */
     static std::shared_ptr <spdlog::logger> getLogger();
 
-    // Delete copy constructor and copy assignment operator to enforce singleton pattern
-    Logger(const Logger &) = delete;
+    ~Logger() = default;
 
+    Logger(const Logger &) = delete;
     Logger &operator=(const Logger &) = delete;
 
 private:
@@ -92,8 +92,6 @@ private:
     static std::string getHostname();
 
     Logger() = default;
-
-    ~Logger() = default;
 };
 
 #endif //CHRONOLOG_LOG_H

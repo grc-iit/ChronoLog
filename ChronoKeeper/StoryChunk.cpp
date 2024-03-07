8c80d4e00013df7da9e543a4a21de39434cce168
Inna Brodkin <ibrodkin@uchicago.edu>
2024-03-07 12:26:40 -0600

    StoryChunk bound: start

--- ChronoKeeper/StoryChunk.cpp
+++ ChronoKeeper/StoryChunk.cpp
@@ -7,7 +7,35 @@ namespace chl = chronolog;
 
 /////////////////////////
 
-uint32_t chronolog::StoryChunk::mergeEvents(std::map <chl::EventSequence, chl::LogEvent> &events
+chl::StoryChunk::StoryChunk(chl::StoryId const &story_id , uint64_t start_time , uint64_t end_time , uint32_t chunk_size )
+            : storyId(story_id)
+            , startTime(start_time)
+            , endTime(end_time)
+            , revisionTime(start_time)
+            , chunkSize(chunk_size)
+    {
+        dataBlob = new char[chunk_size];
+    }
+
+chl::StoryChunk::~StoryChunk()
+    {
+        delete [] dataBlob;
+    }
+
+int chl::StoryChunk::insertEvent(chl::LogEvent const &event)
+    {
+        if((event.time() >= startTime) && (event.time() < endTime))
+        {
+            logEvents.insert(std::pair <chl::EventSequence, chl::LogEvent>({event.time(), event.clientId, event.index()}, event));
+            return 1;
+        }
+        else
+        { return 0; }
+    }
+
+///////////
+
+uint32_t chl::StoryChunk::mergeEvents(std::map <chl::EventSequence, chl::LogEvent> &events
                                             , std::map <chl::EventSequence, chl::LogEvent>::iterator &merge_start)
 {
     uint32_t merged_event_count = 0;
@@ -49,3 +77,23 @@ uint32_t chronolog::StoryChunk::mergeEvents(std::map <chl::EventSequence, chl::L
     return merged_event_count;
 }
 
+uint32_t chl::StoryChunk::mergeEvents(chl::StoryChunk &other_chunk, uint64_t start_time, uint64_t end_time)
+    { return 0; }
+
+uint32_t chl::StoryChunk::extractEvents(std::map <chl::EventSequence, chl::LogEvent> &target_map, std::map <chl::EventSequence, chl::LogEvent>::iterator first_pos
+                  , std::map <chl::EventSequence, chl::LogEvent>::iterator last_pos)
+    { return 0; }
+
+uint32_t chl::StoryChunk::extractEvents(std::map <chl::EventSequence, chl::LogEvent> &target_map, uint64_t start_time, uint64_t end_time)
+    { return 0; }
+
+uint32_t chl::StoryChunk::extractEvents( chl::StoryChunk & target_chunk, uint64_t start_time, uint64_t end_time)
+    { return 0; }
+
+uint32_t chl::StoryChunk::split(chl::StoryChunk & split_chunk, uint64_t time_boundary)
+    { return 0; }
+
+
+uint32_t chl::StoryChunk::eraseEvents(uint64_t start_time, uint64_t end_time)
+    { return 0; }
+ 

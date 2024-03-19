f3eb7636af767c60089fe5e92450d9ec35eaffe8
Kun Feng <kfeng1@hawk.iit.edu>
2024-03-19 15:22:23 -0500

    Update Thallium test code to the latest logger impl

--- test/communication/thallium_client_mpi.cpp
+++ test/communication/thallium_client_mpi.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <iostream>
-#include <iomanip>
 #include <chrono>
 #include <thallium.hpp>
 #include <array>
@@ -11,9 +10,8 @@
 #include "../../ChronoAPI/ChronoLog/include/common.h"
 #include <mpi.h>
 #include "log.h"
-#include "unistd.h"
 
-#define MSG_SIZE 100
+#define MSG_SIZE (1024 * 1024)
 
 namespace tl = thallium;
 using namespace std::chrono;
@@ -24,12 +22,14 @@ void report_results(double duration_ave, double duration_min, double duration_ma
 {
     // Logging the results with descriptive headers for better context.
     LOG_INFO("[Performance Metrics Report]");
-    LOG_INFO("---------------------------------------------------------------");
-    LOG_INFO("{:>20} {:>20} {:>20} {:>20} {:>20} {:>20}", "Average Duration", "Minimum Duration", "Maximum Duration"
-         , "Wall Duration", "Average Initialization Time", "Average Communication Time");
-    LOG_INFO("---------------------------------------------------------------");
-    LOG_INFO("{:>20.2f} {:>20.2f} {:>20.2f} {:>20.2f} {:>20.2f} {:>20.2f}", duration_ave, duration_min, duration_max
-         , duration_wall, duration_init_ave, duration_comm_ave);
+    LOG_INFO(
+            "-------------------------------------------------------------------------------------------------------");
+    LOG_INFO("{:>16} {:>16} {:>16} {:>16} {:>16} {:>16}", "Ave Time", "Min Time", "Max Time", "Wall Time"
+             , "Ave Init Time", "Ave Comm Time");
+    LOG_INFO(
+            "-------------------------------------------------------------------------------------------------------");
+    LOG_INFO("{:>16.4f} {:>16.4f} {:>16.4f} {:>16.4f} {:>16.4f} {:>16.4f}", duration_ave, duration_min, duration_max
+             , duration_wall, duration_init_ave, duration_comm_ave);
 }
 
 
@@ -56,7 +56,7 @@ void calculate_time(time_point <my_clock, nanoseconds> &t_bigbang, time_point <m
     duration_ave /= nprocs;
     duration_init_ave /= nprocs;
     duration_comm_ave /= nprocs;
-    duration_comm_ave /= repetition;
+    duration_comm_ave /= (double)repetition;
 }
 
 std::string get_server_address(const std::string &base_address, long num_servers, int rank)
@@ -64,11 +64,7 @@ std::string get_server_address(const std::string &base_address, long num_servers
     std::string host_ip = base_address.substr(0, base_address.rfind(':'));
     std::string base_port = base_address.substr(base_address.rfind(':') + 1);
     // randomly select target server port
-    long max_port = stoi(base_port) + num_servers - 1;
-    //  std::uniform_int_distribution<long> distr(stoi(base_port), max_port);
-    //  long new_port = distr(mt);
-    // round-robin select target server port
-    int new_port = atoi(base_port.c_str()) + rank % num_servers;
+    auto new_port = strtol(base_port.c_str(), nullptr, 10) + rank % num_servers;
     LOG_DEBUG("[ThalliumClientMPI] Selected server port based on rank {}: {}", rank, new_port);
     std::string new_addr_str = host_ip + ":" + std::to_string(new_port);
     LOG_INFO("[ThalliumClientMPI] Generated server address for engine: {}", new_addr_str);
@@ -79,7 +75,7 @@ int main(int argc, char**argv)
 {
     if(argc < 4)
     {
-        LOG_ERROR("[ThalliumClientMPI] Usage: {} <address> <#servers> <sendrecv|rdma> [repetition]", argv[0]);
+        std::cerr << "Usage: " << argv[0] << " <address> <#servers> <sendrecv|rdma> [repetition]" << std::endl;
         exit(0);
     }
 
@@ -87,6 +83,13 @@ int main(int argc, char**argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    int result = Logger::initialize("console", "thallium_client_mpi.log", spdlog::level::debug, "thallium_client_mpi"
+                                    , 1048576, 5, spdlog::level::debug);
+    if(result == 1)
+    {
+        exit(EXIT_FAILURE);
+    }
 
     std::string base_address = argv[1];
     long num_servers = strtol(argv[2], nullptr, 10);
@@ -103,7 +106,7 @@ int main(int argc, char**argv)
                                    "abcdefghijklmnopqrstuvwxyz";
     for(int i = 0; i < MSG_SIZE; ++i)
     {
-        send_vec[i] = alphanum[dist(mt) % (sizeof(alphanum) - 1)];
+        send_vec.push_back(alphanum[dist(mt) % (sizeof(alphanum) - 1)]);
     }
     double duration_min, duration_max, duration_ave, duration_wall;
     double duration_init_ave, duration_comm_ave;
@@ -118,13 +121,19 @@ int main(int argc, char**argv)
         server_address = get_server_address(base_address, num_servers, my_rank);
         std::string rpc_name = "repeater";
         LOG_DEBUG("[ThalliumClientMPI] Attempting to establish RPC connection for sendrecv mode with server at: {}"
-             , server_address);
+                  , server_address);
         tl::remote_procedure repeater = myEngine.define(rpc_name);
         tl::endpoint server = myEngine.lookup(server_address);
         tl::provider_handle ph(server);
         t_local_init = my_clock::now();
         for(int i = 0; i < repetition; i++)
-            repeater.on(server)(send_vec);
+        {
+            ret_vec = repeater.on(server)(send_vec).as <std::vector <char>>();
+//            if(ret_vec != send_vec)
+//            {
+//                LOG_ERROR("[ThalliumClientMPI] Mismatch detected: The server's response does not match the sent data.");
+//            }
+        }
         t_local_finish = my_clock::now();
         MPI_Barrier(MPI_COMM_WORLD);
         t_global_finish = my_clock::now();
@@ -138,12 +147,13 @@ int main(int argc, char**argv)
         server_address = get_server_address(base_address, num_servers, my_rank);
         std::string rpc_name = "rdma_put";
         LOG_DEBUG("[ThalliumClientMPI] Attempting to establish RPC connection for RDMA mode with server at: {}"
-             , server_address);
-        tl::remote_procedure rdma_put = myEngine.define(rpc_name).disable_response();
+                  , server_address);
+        tl::remote_procedure rdma_put = myEngine.define(rpc_name); //.disable_response();
         tl::endpoint server = myEngine.lookup(server_address);
         std::vector <std::pair <void*, std::size_t>> segments(1);
         segments[0].first = (void*)(&send_vec[0]);
         segments[0].second = send_vec.size() + 1;
+        LOG_DEBUG("[ThalliumClientMPI] Exposing memory segments of size {} for RDMA mode.", send_vec.size() + 1);
         tl::bulk myBulk = myEngine.expose(segments, tl::bulk_mode::read_only);
         t_local_init = my_clock::now();
         for(int i = 0; i < repetition; i++)
@@ -163,10 +173,6 @@ int main(int argc, char**argv)
 
     if(my_rank == 0)
     {
-        if(ret_vec != send_vec)
-        {
-            LOG_ERROR("[ThalliumClientMPI] Mismatch detected: The server's response does not match the sent data.");
-        }
         report_results(duration_ave, duration_min, duration_max, duration_wall, duration_init_ave, duration_comm_ave);
     }
 

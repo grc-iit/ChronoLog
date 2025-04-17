f8a44bbb0158b00ce1ba9066459311c99078e333
EnekoGonzalez3 <egonzalez30@iit.edu>
2025-04-17 21:04:50 +0000

    fix: include file not error codes.

--- ChronoAPI/ChronoLog/include/chronolog_errcode.h
+++ ChronoAPI/ChronoLog/include/chronolog_errcode.h
@@ -1,25 +1,12 @@
 #ifndef CHRONOLOG_CHRONOLOG_ERRCODE_H
 #define CHRONOLOG_CHRONOLOG_ERRCODE_H
 
-//#include "Chronolog/Client/include/client_errorcodes.h"
+#include "../../../Client/include/client_errcode.h"
 
 namespace chronolog
 {
 enum ErrorCode
 {
-    // ---------- Client error codes ----------
-    CL_SUCCESS = 0,                             // Success
-    CL_ERR_UNKNOWN = -1,                        // Generic error
-    CL_ERR_INVALID_ARG = -2,                    // Invalid input
-    CL_ERR_NOT_EXIST = -3,                      // Missing Chronicle, Story, or property
-    CL_ERR_ACQUIRED = -4,                       // Already acquired, cannot destroy
-    CL_ERR_NOT_ACQUIRED = -5,                   // Not acquired, cannot release
-    CL_ERR_CHRONICLE_EXISTS = -6,               // Chronicle already exists
-    CL_ERR_NO_KEEPERS = -7,                     // No ChronoKeepers available
-    CL_ERR_NO_CONNECTION = -8,                  // No connection to ChronoVisor
-    CL_ERR_NOT_AUTHORIZED = -9,                 // Unauthorized operation
-    CL_ERR_NO_PLAYERS = -10,                     // No ChronoPlayers available
-
     // ---------- Server-only error codes (must be <-100) ----------
     CL_ERR_STORY_EXISTS = -101,                 // Specified Story exists, cannot be created/renamed to
     CL_ERR_Archive_EXISTS = -102,               // Specified Archive exists, cannot be created/renamed to

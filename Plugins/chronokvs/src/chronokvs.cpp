1264f4cd03c1ed00e5e9da1d6d7a3e99e90556b7
EnekoGonzalez3 <egonzalez30@iit.edu>
2025-09-08 17:34:27 +0000

    feat: define the new API

--- Plugins/chronokvs/src/chronokvs.cpp
+++ Plugins/chronokvs/src/chronokvs.cpp
@@ -15,19 +15,13 @@ std::uint64_t ChronoKVS::put(const std::string &key, const std::string &value)
     return mapper->storeKeyValue(key, value);
 }
 
-std::vector <std::pair <std::string, std::string>> ChronoKVS::get(std::uint64_t timestamp)
-{
-    return mapper->retrieveByTimestamp(timestamp);
-}
-
-std::vector <std::pair <std::uint64_t, std::string>> ChronoKVS::get(const std::string &key)
+std::vector <std::pair <std::uint64_t, std::string>> ChronoKVS::get_history(const std::string &key)
 {
     return mapper->retrieveByKey(key);
 }
 
 std::string ChronoKVS::get(const std::string &key, std::uint64_t timestamp)
 {
-    return mapper->retrieveByKeyAndTimestamp(key, timestamp);
+    return mapper->retrieveByKeyAndTs(key, timestamp);
 }
-
 }

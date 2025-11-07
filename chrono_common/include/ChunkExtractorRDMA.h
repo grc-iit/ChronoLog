9db96ccb571be621c624bc465e44443673dd2d89
Inna Brodkin <ibrodkin@uchicago.edu>
2025-11-07 17:25:47 -0600

    added added StoryChunkConsumerService test & a few bug fixes

--- chrono_common/include/ChunkExtractorRDMA.h
+++ chrono_common/include/ChunkExtractorRDMA.h
@@ -4,6 +4,8 @@
 #include <chronolog_types.h>
 #include <ServiceId.h>
 
+namespace tl = thallium;
+
 namespace chronolog
 {
 
@@ -15,13 +17,18 @@ class StoryChunkExtractorRDMA
 
 public:
     StoryChunkExtractorRDMA( tl::engine & tl_engine, ServiceId const & service_id);
+    StoryChunkExtractorRDMA( StoryChunkExtractorRDMA const & other);
+    StoryChunkExtractorRDMA & operator=( StoryChunkExtractorRDMA const & other); 
 
     ~StoryChunkExtractorRDMA();
 
+    tl::engine & get_sender_engine() const { return sender_tl_engine;}
+    ServiceId const& get_receiver_service_id() const { return receiver_service_id; }
     int process_chunk(StoryChunk*);
 
 private:
     tl::engine & sender_tl_engine;         // local tl::engine
+    ServiceId receiver_service_id;          // receiving ServiceId
     RDMATransferAgent * rdma_sender;
 };
 
--- chrono_common/src/RDMATransferAgent.cpp
+++ chrono_common/src/RDMATransferAgent.cpp
@@ -15,7 +15,6 @@ chronolog::RDMATransferAgent::RDMATransferAgent(tl::engine &tl_engine, chronolog
     std::string service_addr_string;
     receiver_service_id.get_service_as_string(service_addr_string);
     
-    LOG_DEBUG("[RDMATransferAgent] Constructor for receiver service {} , service_string {}", chl::to_string(receiver_service_id), service_addr_string);
     receiver_service_handle = tl::provider_handle(service_engine.lookup(service_addr_string), receiver_service_id.getProviderId());
 
     receiver_is_available = service_engine.define("receiver_is_available");
@@ -26,130 +25,55 @@ chronolog::RDMATransferAgent::RDMATransferAgent(tl::engine &tl_engine, chronolog
 
 chronolog::RDMATransferAgent::~RDMATransferAgent()
 {
+    LOG_DEBUG("[RDMATransferAgent] Destroying agent for receiver service {}", chl::to_string(receiver_service_id));
     receiver_is_available.deregister();
     receive_story_chunk.deregister();
-    LOG_DEBUG("[RDMATransferAgent] Destroying agent for receiver service {}", chl::to_string(receiver_service_id));
 }
 
 bool chronolog::RDMATransferAgent::is_receiver_available() const
 {
-    bool ret_value = receiver_is_available.on(receiver_service_handle)( );
+    bool ret_value = false;
+    try
+    {
+        ret_value = receiver_is_available.on(receiver_service_handle)( );
+    }
+    catch(...)
+    {
+        LOG_ERROR("[RDMATransferAgent] Unknown exception in rpc with receiver_service {}.", chl::to_string(receiver_service_id));
+    }
 
     LOG_DEBUG("[RDMATransferAgent] receiver_service {} is available {}", chl::to_string(receiver_service_id), ret_value);
 return ret_value;    
 }
 
-/*int chronolog::RDMATransferAgent::transfer_story_chunk(chronolog::StoryChunk*story_chunk)
-{
-    try
-    {
-        LOG_DEBUG("[RDMATransferAgent] agent for receiver {} transfering story chunk, StoryID: {}, StartTime: {}, event_count {}", chl::to_string(receiver_service_id)
-                  , story_chunk->getStoryId(), story_chunk->getStartTime(), story_chunk->getEventCount());
-
-#ifdef LOGTIME
-        std::chrono::high_resolution_clock::time_point start, end;
-        start = std::chrono::high_resolution_clock::now();
-#endif
-        size_t serialized_story_chunk_size;
-        std::ostringstream oss(std::ios::binary);
-        cereal::BinaryOutputArchive oarchive(oss);
-        oarchive(*story_chunk);
-        std::string serialized_story_chunk = oss.str();
-        serialized_story_chunk_size = serialized_story_chunk.size();
-
-#ifdef LOGTIME
-        end = std::chrono::high_resolution_clock::now();
-        LOG_INFO("[RDMATransferAgent] StoryChunk serialization took {} us",
-                std::chrono::duration_cast <std::chrono::nanoseconds>(end - start).count() / 1000.0);
-#endif
-        LOG_DEBUG("[RDMATransferAgent] Serialized StoryChunk size: {}", serialized_story_chunk_size);
-
-        std::vector <std::pair <void*, std::size_t>> segments(1);
-        segments[0].first = (void*)(serialized_story_chunk.data());
-        segments[0].second = serialized_story_chunk_size;
-        tl::bulk tl_bulk = service_engine.expose(segments, tl::bulk_mode::read_only);
-        LOG_DEBUG("[RDMATransferAgent] Draining StoryChunk size: {} ...", tl_bulk.size());
-
-        size_t bytes_transfered = receive_story_chunk.on(receiver_service_handle)(tl_bulk);
-
-#ifdef LOGTIME 
-        start = end;
-        end = std::chrono::high_resolution_clock::now();
-        LOG_INFO("[RDMATransferAgent] StoryChunk transfer took {} us",
-                std::chrono::duration_cast <std::chrono::nanoseconds>(end - start).count() / 1000.0);
-#endif
-        LOG_DEBUG("[RDMATransferAgent] StoryChunk transfer returned with result: {}", bytes_transfered);
-
-        if(bytes_transfered == serialized_story_chunk_size)
-        {
-            LOG_INFO("[RDMATransferAgent] Successfully transfered StoryChunk, StoryId:{}, StartTime:{}", story_chunk->getStoryId(), story_chunk->getStartTime());
-            return chronolog::CL_SUCCESS;
-        }
-    }
-    catch(tl::exception const &ex)
-    {
-        LOG_ERROR("[RDMATransferAgent] Thallium exception while transferring StoryChunk: {}", ex.what());
-        return (chronolog::CL_ERR_UNKNOWN);
-    }
-    catch(cereal::Exception const &ex)
-    {
-        LOG_ERROR("[RDMATransferAgent] Cereal exception while serializing StoryChunk: {}", ex.what());
-        return chronolog::CL_ERR_UNKNOWN;
-    }
-    catch(std::exception const &ex)
-    {
-        LOG_ERROR("[RDMATransferAgent] Standard exception while serializing StoryChunk: {}", ex.what());
-        return chronolog::CL_ERR_UNKNOWN;
-    }
-    catch(...)
-    {
-        LOG_ERROR("[RDMATransferAgent] Unknown exception while  transferring StoryChunk.");
-        return chronolog::CL_ERR_UNKNOWN;
-    }
-
-    LOG_ERROR("[RDMATransferAgent] Failed to transfer StoryShunk, StoryId:{},StartTime:{}", story_chunk->getStoryId(), story_chunk->getStartTime());
-    return chronolog::CL_ERR_STORY_CHUNK_EXTRACTION;
-}
-*/
 ///////////////////////////////////
 int chronolog::RDMATransferAgent::transfer_serialized_bulk( std::string const& serialized_story_chunk)
 {
     try
     {
-        size_t serialized_story_chunk_size;
-        //std::ostringstream oss(std::ios::binary);
-        //cereal::BinaryOutputArchive oarchive(oss);
-        //oarchive(*story_chunk);
-       // std::string serialized_story_chunk = oss.str();
-        serialized_story_chunk_size = serialized_story_chunk.size();
-
-        //LOG_DEBUG("[RDMATransferAgent] Serialized StoryChunk size: {}", serialized_story_chunk_size);
-
         std::vector <std::pair <void*, std::size_t>> segments(1);
         segments[0].first = (void*)(serialized_story_chunk.data());
-        segments[0].second = serialized_story_chunk_size;
+        segments[0].second = serialized_story_chunk.size();
         tl::bulk tl_bulk = service_engine.expose(segments, tl::bulk_mode::read_only);
-        LOG_DEBUG("[RDMATransferAgent] transfering Chunk size: {}  tl_bulk size ...",  serialized_story_chunk_size, tl_bulk.size());
+        LOG_TRACE("[RDMATransferAgent] about to transfer Chunk size: {}  tl_bulk size {}",  serialized_story_chunk.size(), tl_bulk.size());
 
         size_t bytes_transfered = receive_story_chunk.on(receiver_service_handle)(tl_bulk);
 
-        LOG_DEBUG("[RDMATransferAgent] sent tl_bulk size {} transfered {} bytes", tl_bulk.size(), bytes_transfered);
+        LOG_DEBUG("[RDMATransferAgent] prepared tl_bulk size {} transfered {} bytes", tl_bulk.size(), bytes_transfered);
 
-        if(bytes_transfered == serialized_story_chunk_size)
+        if(bytes_transfered == tl_bulk.size())
         {
-           // LOG_INFO("[RDMATransferAgent] Successfully transfered StoryChunk, StoryId:{}, StartTime:{}", story_chunk->getStoryId(), story_chunk->getStartTime());
+            LOG_TRACE("[RDMATransferAgent] Successfully transfered bulk");
             return chronolog::CL_SUCCESS;
         }
     }
     catch(tl::exception const &ex)
     {
-        LOG_ERROR("[RDMATransferAgent] Thallium exception while transferring StoryChunk: {}", ex.what());
-        return (chronolog::CL_ERR_UNKNOWN);
+        LOG_ERROR("[RDMATransferAgent] Thallium exception while transferring bulk: {}", ex.what());
     }
     catch(...)
     {
-        LOG_ERROR("[RDMATransferAgent] Unknown exception while  transferring StoryChunk.");
-        return chronolog::CL_ERR_UNKNOWN;
+        LOG_ERROR("[RDMATransferAgent] Unknown exception while  transferring bulk.");
     }
 
     return chronolog::CL_ERR_STORY_CHUNK_EXTRACTION;
